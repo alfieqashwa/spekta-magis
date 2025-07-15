93c90087973fda1a2ce7be0296f23c0100a8ba3a
Alfie Qashwa <alfieqashwa@gmail.com>
2025-07-16 02:28:51 +0700

    fix: animate

--- components/countdown-timer.tsx
+++ components/countdown-timer.tsx
@@ -3,7 +3,8 @@
 import { useState } from "react";
 import Countdown, { CountdownRendererFn } from "react-countdown";
 import { Button } from "@/components/ui/button";
-import { parse, formatISO } from "date-fns";
+import { parse } from "date-fns";
+import { motion } from "motion/react";
 
 const getEventDate = () => {
   // Local date in UTC+07:00, convert to UTC by subtracting 7 hours
@@ -35,43 +36,46 @@ export default function CountdownTimer() {
 
     return (
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 text-center font-bold tracking-widest">
-        <div className="flex flex-col items-center justify-center rounded-xl border border-primary/10 bg-white/5 p-4 backdrop-blur-md transform-gpu glass [box-shadow:0_-20px_80px_-20px_#ff7aa42f_inset]">
-          <div className="text-3xl text-rose-500 font-mono">
-            {String(days).padStart(2, "0")}
-          </div>
-          <div className="text-sm uppercase text-muted-foreground">Days</div>
-        </div>
-        <div className="flex flex-col items-center justify-center rounded-xl border border-primary/10 bg-white/5 p-4 backdrop-blur-md glass">
-          <div className="text-3xl font-mono">
-            {String(hours).padStart(2, "0")}
-          </div>
-          <div className="text-sm uppercase text-muted-foreground">Hours</div>
-        </div>
-        <div className="flex flex-col items-center justify-center rounded-xl border border-primary/10 bg-white/5 p-4 backdrop-blur-md glass">
-          <div className="text-3xl textmute font-mono">
-            {String(minutes).padStart(2, "0")}
-          </div>
-          <div className="text-sm uppercase text-muted-foreground">Min</div>
-        </div>
-        <div className="flex flex-col items-center justify-center rounded-xl border border-primary/10 bg-white/5 p-4 backdrop-blur-md glass">
-          <div className="text-3xl font-mono">
-            {String(seconds).padStart(2, "0")}
-          </div>
-          <div className="text-sm text-muted-foreground uppercase">Sec</div>
-        </div>
+        <CountdownCard context={String(days).padStart(2, "0")} title="Days" />
+        <CountdownCard context={String(hours).padStart(2, "0")} title="Hours" />
+        <CountdownCard context={String(minutes).padStart(2, "0")} title="Min" />
+        <CountdownCard context={String(seconds).padStart(2, "0")} title="Sec" />
       </div>
     );
   };
 
   return (
     <div className="dark:text-white p-6 rounded-lg shadow-lg">
-      <article className="mb-4 font-semibold text-center">
+      <motion.article
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.7 }}
+        className="mb-4 font-semibold text-center"
+      >
         <h1 className="text-lg">20 September 2025</h1>
         <h1 className="text-muted-foreground">
           Kalawa Convention Hall, Palangka Raya
         </h1>
-      </article>
+      </motion.article>
       <Countdown date={targetDate} renderer={renderer} />
     </div>
   );
 }
+
+const CountdownCard = ({
+  context,
+  title,
+}: {
+  context: string;
+  title: string;
+}) => (
+  <motion.div
+    initial={{ opacity: 0, scale: "70%" }}
+    animate={{ opacity: 1, scale: "100%" }}
+    transition={{ duration: 1, delay: 0.7 }}
+    className="flex flex-col items-center justify-center rounded-xl border border-primary/10 bg-white/5 p-4 backdrop-blur-md transform-gpu glass [box-shadow:0_-20px_80px_-20px_#ff7aa42f_inset]"
+  >
+    <div className="text-3xl text-rose-500 font-mono">{context}</div>
+    <div className="text-sm uppercase text-muted-foreground">{title}</div>
+  </motion.div>
+);
