c852156c3e96ce08635d80705fc5aed9ff078684
Alfie Qashwa <alfieqashwa@gmail.com>
2025-07-17 01:57:55 +0700

    fix: modified footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,33 @@
+import { CONTACT, INSTAGRAM } from "@/constants/contact";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 export const Footer = () => (
-  <div className="absolute bottom-0">
+  <div className="absolute bottom-0 z-10">
     <div className="flex gap-4 justify-end items-center p-4">
-      <section className="flex items-center space-x-2">
-        <FaInstagram className="text-rose-500 size-6" />
-        <span className="text-sm font-medium">Spektamagis</span>
+      <section>
+        <a
+          href={INSTAGRAM}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center space-x-2"
+        >
+          <FaInstagram className="text-[#EB2D2E] size-6 animate-pulse-slow" />
+          <span className="text-xs md:text-sm font-medium">Spektamagis</span>
+        </a>
       </section>
-      <p>|</p>
-      <section className="flex items-center space-x-2">
-        <FaWhatsapp className="text-rose-500 size-6" />
-        <span className="text-sm font-medium">0852 85 999 565</span>
+      <a className="text-gray-300">|</a>
+      <section>
+        <a
+          href={CONTACT}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center space-x-2"
+        >
+          <FaWhatsapp className="text-[#EB2D2E] animate-pulse-slow size-6" />
+          <span className="text-xs md:text-sm font-medium">
+            0852 85 999 565
+          </span>
+        </a>
       </section>
     </div>
   </div>
---
+++ constants/contact.ts
@@ -0,0 +1,4 @@
+const _SPACE = "%20";
+export const PHONE = "6285285999565";
+export const CONTACT = `https://wa.me/${PHONE}`;
+export const INSTAGRAM = `https://instagram.com/spektamagis`;
