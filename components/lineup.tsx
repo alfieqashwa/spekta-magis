288942517622f7d5f18d2ff245c97601a51cbd8c
Alfie Qashwa <alfieqashwa@gmail.com>
2025-07-20 15:09:26 +0700

    style: reconfig grid for project pop

--- components/lineup.tsx
+++ components/lineup.tsx
@@ -37,11 +37,11 @@ export function LineUp(props: LineUpProps) {
         >
           Line Up
         </h2>
-        <p className="mx-4 mt-5 text-center text-lg text-zinc-500">
+        <p className="mx-4 mt-5 text-center text-lg text-zinc-500 md:text-xl">
           Every Beat Has a Name <br className="md:hidden" /> — Meet the Artists.
         </p>
       </motion.div>
-      <div className="mt-12 grid w-full grid-cols-1 gap-x-8 gap-y-8 text-center md:grid-cols-4 lg:gap-y-12">
+      <div className="mt-12 mb-12 grid w-full grid-cols-1 gap-8 text-center md:grid-cols-6 lg:gap-12">
         {props.cards.map((card, i) => (
           <motion.div
             key={i}
@@ -49,7 +49,9 @@ export function LineUp(props: LineUpProps) {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 1.25 }}
             className={cn(
-              card.title === "Project Pop" ? "md:col-span-4" : "md:col-span-2",
+              card.title === "Project Pop"
+                ? "md:col-start-2 md:col-end-6"
+                : "md:col-span-3",
             )}
           >
             <CardHoverEffect
