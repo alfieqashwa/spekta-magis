4904a25e335667107a677a4fd0a37f28be511a87
Alfie Qashwa <alfieqashwa@gmail.com>
2025-07-24 22:09:35 +0700

    feat: add more constant soc-med

--- constants/contact.ts
+++ constants/contact.ts
@@ -2,3 +2,5 @@ const _SPACE = "%20";
 export const PHONE = "6285285999565";
 export const CONTACT = `https://wa.me/${PHONE}`;
 export const INSTAGRAM = `https://instagram.com/spektamagis`;
+export const TIKTOK = `https://tiktok.com/@spektamagis_`;
+export const FACABOOK = `https://www.facebook.com/spektamagis`;
