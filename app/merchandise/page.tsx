301e2c509e61efa6033a957f4554c3ae9001d9c1
Alfie Qashwa <alfieqashwa@gmail.com>
2025-07-31 01:03:25 +0700

    fix: updated merchandise images

--- app/merchandise/page.tsx
+++ app/merchandise/page.tsx
@@ -7,15 +7,15 @@ export const metadata: Metadata = {
 
 export default function MerchandisePage() {
   const images = [
-    { src: "/merchandise/gelang.jpg", alt: "Gelang" },
-    { src: "/merchandise/sticker.jpg", alt: "Sticker" },
+    { src: "/merchandise/gelang.png", alt: "Gelang" },
     { src: "/merchandise/t-shirt1.png", alt: "T-shirt 1" },
     { src: "/merchandise/t-shirt2.png", alt: "T-shirt 2" },
     { src: "/merchandise/t-shirt3.png", alt: "T-shirt 3" },
     { src: "/merchandise/t-shirt4.png", alt: "T-shirt 4" },
+    { src: "/merchandise/tumbler.jpg", alt: "Tumbler" },
+    { src: "/merchandise/sticker.jpg", alt: "Sticker" },
     { src: "/merchandise/tote-bag-1.jpg", alt: "Tote Bag 2" },
     { src: "/merchandise/tote-bag-2.jpg", alt: "Tote Bag 2" },
-    { src: "/merchandise/tumbler.jpg", alt: "Tumbler" },
   ];
   return <Gallery images={images} />;
 }
