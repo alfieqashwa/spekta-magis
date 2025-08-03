e3049ff97fb20d34df3b040b880ea3ab36a627bf
M Zulfikar Septriawan <45314424+mzulseptriawan@users.noreply.github.com>
2025-08-03 12:37:08 +0700

    chore: update Facebook link to new url
    Change link fb to https://facebook.com/61578793045913

--- constants/contact.ts
+++ constants/contact.ts
@@ -3,7 +3,7 @@ export const PHONE = "6285285999565";
 export const CONTACT = `https://wa.me/${PHONE}`;
 export const INSTAGRAM = `https://instagram.com/spektamagis`;
 export const TIKTOK = `https://tiktok.com/@spektamagis_`;
-export const FACEBOOK = `https://www.facebook.com/people/Spektamagis/61578506602137/`;
+export const FACEBOOK = `https://facebook.com/61578793045913`;
 
 // <div className="flex items-center justify-around space-x-3 px-4">
 //   <section>
